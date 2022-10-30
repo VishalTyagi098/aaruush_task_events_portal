99a0cefb6a54861c6ef9921803f02a9f0ccfd812
vishal <vishaltyagi.dev098@gmail.com>
2022-10-30 13:03:44 +0530

    FE done

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,16 +2,41 @@ import React from 'react'
 import RegisterCard from './RegisterCard'
 import './Register.css'
 
+import monoact from '../images/monoact.webp'
+import reroute from '../images/reroute.webp'
+import roboleague from '../images/roboleague.webp'
+
 function Register() {
+  const registerCardData1 ={
+    image:monoact,
+    title:"MonoAct",
+    desc: "To bring out the artist in you. A chance to be an entire crew. Actors! Directors! Filmmakers! we have all got you covered",
+    fees:"Included with CR",
+    link:"#",
+  }
+  const registerCardData2 ={
+    image:reroute,
+    title:"Reroute",
+    desc: "Mirroring the human movements and incorporating them into the autonomous world, Team Aaruush presents to you, ‘Reroute- Line follower Challenge’ flagship of Domain Online. ",
+    fees:"INR 150",
+    link:"#",
+  }
+  const registerCardData3 ={
+    image:roboleague,
+    title:"Robo League",
+    desc: "Experience the adrenaline and test the strength, stability, accuracy and speed of your robot",
+    fees:"INR 250",
+    link:"#",
+  }
   return (
     <div className='register-container'>
       <div className="register-title">
         Register
       </div>
       <div className="register-cards">
-        <RegisterCard/>
-        <RegisterCard/>
-        <RegisterCard/>
+        <RegisterCard {...registerCardData1}/>
+        <RegisterCard {...registerCardData2}/>
+        <RegisterCard {...registerCardData3}/>
       </div>
       
     </div>
--- src/components/RegisterCard.js
+++ src/components/RegisterCard.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import './RegisterCard.css'
-import monoact from '../images/monoact.webp'
 
 
-function RegisterCard() {
+function RegisterCard(props) {
   return (
     <div className='register-card-container'>
       <div className="img-container">
-        <img src={monoact} alt="" height={500}/>
+        <img src={props.image} alt="" height={500}/>
       </div>
 
       <div className="text-container">
-        <div className="text-title">Monoact</div>
-        <div className="text-para">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus assumenda doloribus quas itaque, cupiditate tempora asperiores quibusdam blanditiis voluptatum nam aliquam iusto. Et, perferendis! Doloremque.</div>
-        <div className="text-fees">included with CR</div>
+        <div className="text-title">{props.title}</div>
+        <div className="text-para">{props.desc}</div>
+        <div className="text-fees">{props.fees}</div>
         <div className="text-btn">
           <button className='register-btn'>Register</button>
         </div>
